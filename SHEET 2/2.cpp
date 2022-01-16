4a472dac13c4c6a50d2489c4a9447b6d17d96e2d
Vaishali2021 <72402488+Vaishali2021@users.noreply.github.com>
2022-01-16 22:26:06 +0530

    Codes

---
+++ SHEET 2/2.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    int second=1;
+    int third=first+second;
+    int k=n*(n+1)/2;
+    int arr[k+1];
+    arr[0]=first;
+    arr[1]=second;
+    for(int i = 2;i<=n*(n+1)/2;i++)
+    {
+        third=first+second;
+        first=second;
+        second=third;
+        arr[i]=third;
+    }
+    int p=0;
+    for(int i =1;i<=n;i++)
+    {
+        for(int j=1;j<=i;j++)
+        {
+            cout<<arr[p++]<<" ";
+        }
+        cout<<"\n";
+    }
+    return 0;
+}
